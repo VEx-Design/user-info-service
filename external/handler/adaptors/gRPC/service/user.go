cd6007c4c5e5aba48c7c4286d8646b7b1bf30a36
Atipat D <95071528+Atipat-CMU@users.noreply.github.com>
2025-01-10 12:01:17 +0700

    hotfix: handle user not found

--- external/handler/adaptors/gRPC/service/user.go
+++ external/handler/adaptors/gRPC/service/user.go
@@ -3,6 +3,7 @@ package grpc_service
 import (
 	"context"
 	"errors"
+	"fmt"
 
 	pb "user-info-service/external/handler/adaptors/gRPC"
 	"user-info-service/internal/core/logic"
@@ -18,8 +19,13 @@ func (s *UserServiceGRPC) GetUser(ctx context.Context, req *pb.GetUserRequest) (
 		return nil, errors.New("user ID is required")
 	}
 
+	// Fetch the user from your user service
 	user, err := s.UserSrv.GetUser(req.GetUserId())
+	if err != nil {
+		return nil, fmt.Errorf("failed to get user: %w", err)
+	}
 
+	// Return the user information
 	return &pb.GetUserResponse{
 		User: &pb.User{
 			Id:      user.ID,
@@ -27,5 +33,5 @@ func (s *UserServiceGRPC) GetUser(ctx context.Context, req *pb.GetUserRequest) (
 			Email:   user.Email,
 			Picture: user.Picture,
 		},
-	}, err
+	}, nil
 }
